83416ce8d3d807f503c3191b7b5f5d97dbd9b87e
iltaier <mohabziny@gmail.com>
2022-01-06 16:37:00 +0200

    solving bugs

--- src/Controller/ClientHandler.java
+++ src/Controller/ClientHandler.java
@@ -22,6 +22,7 @@ import model.Player;
 import model.Register;
 import model.TopOnlinePlayers;
 import model.LogOut;
+import model.Move;
 import model.Request;
 
 /**
@@ -47,9 +48,11 @@ public class ClientHandler extends Thread {
 
     public ClientHandler(Socket cs) {
         try {
+            socket = cs;
             os = cs.getOutputStream();
             is = cs.getInputStream();
-            socket = cs;
+            ois = new ObjectInputStream(is);
+            oos = new ObjectOutputStream(os);
             start();
         } catch (IOException ex) {
             Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
@@ -61,102 +64,105 @@ public class ClientHandler extends Thread {
         db = new DBConnection();
         while (true) {
             try {
-                ois = new ObjectInputStream(is);
-                if (ois != null) {
-                    Object readObj = ois.readObject();
-                    System.out.println(readObj.toString());
-                    if (readObj instanceof Login) {
-                        oos = new ObjectOutputStream(os);
-                        try {
+                Object readObj = ois.readObject();
+                System.out.println(readObj.toString());
+                if (readObj instanceof Login) {
 
-                            Player login = db.loginCheck((Login) readObj);
-                            if (login != null) {
-                                login.setIsOnline(1);
-                                login.setIsRequest(1);
-                                db.changeOnlineStatus(login);
-                                db.inGameStatus(login);
-                                Player player = login;
-                                player = db.getPlayerInformation(login);
-                                clientsVector.put(player.getUserName(), this);
-                                System.out.println("No of Clients: " + clientsVector.size());
-                                oos.writeObject(player);
-                                oos.flush();
-                            } else {
-                                oos.writeObject("Login Error");
-                                oos.flush();
-                            }
+                    try {
 
-                        } catch (SQLException ex) {
-                            Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+                        Player login = db.loginCheck((Login) readObj);
+                        if (login != null) {
+                            login.setIsOnline(1);
+                            login.setIsRequest(1);
+                            db.changeOnlineStatus(login);
+                            db.inGameStatus(login);
+                            Player player = login;
+                            player = db.getPlayerInformation(login);
+                            clientsVector.put(player.getUserName(), this);
+                            System.out.println("No of Clients: " + clientsVector.size());
+                            oos.writeObject(player);
+                            oos.flush();
+                        } else {
                             oos.writeObject("Login Error");
                             oos.flush();
                         }
 
-                    } else if (readObj instanceof Register) {
-                        oos = new ObjectOutputStream(os);
-                        Register r = (Register) readObj;
-                        System.out.println("userName: " + r.getUserName() + ", pass: " + r.getPassward());
-                        try {
-                            Player p = db.registerNewPlayer(r);
-                            oos.writeObject(p);
-                            oos.flush();
-                        } catch (SQLException sQLException) {
-                            System.out.println(sQLException.toString());
-                            oos.writeObject("Error");
-                            oos.flush();
-                        }
-                    } else if (readObj instanceof TopOnlinePlayers) {
-                        oos = new ObjectOutputStream(os);
+                    } catch (SQLException ex) {
+                        Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+                        oos.writeObject("Login Error");
+                        oos.flush();
+                    }
 
-                        TopOnlinePlayers topOnlinePlayers = (TopOnlinePlayers) readObj;
-                        try {
-                            String str = topOnlinePlayers.getUserName();
-                            topOnlinePlayers.setOnlinePlayers(db.getOnlinePlayers(str));
-                            topOnlinePlayers.setTopPlayers(db.getTopPlayers());
-                            oos.writeObject(topOnlinePlayers);
-                            oos.flush();
-                        } catch (SQLException sQLException) {
-                            System.out.println(sQLException.toString());
-                            oos.writeObject("error 4");
-                            oos.flush();
-                        }
-                    } else if (readObj instanceof LogOut) {
-                        Player p = new Player();
-                        LogOut logOut = (LogOut) readObj;
-                        System.out.println(logOut.getUserName());
-                        p.setUserName(logOut.getUserName());
-                        p.setIsRequest(0);
-                        p.setIsOnline(0);
-                        try {
-                            System.out.println("in try");
-                            db.changeOnlineStatus(p);
-                            db.inGameStatus(p);
-                            oos = new ObjectOutputStream(os);
-                            System.out.println(oos.toString());
-                            oos.writeObject("LoggedOut");
-                            System.out.println(socket);
-                            oos.flush();
-                            ois.close();
-                            oos.close();
-                            socket.close();
-                            clientsVector.remove(p.getUserName());
-                        } catch (SQLException ex) {
-                            Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
-                            System.out.println("something wrong");
-                        }
-                    } else if (readObj instanceof Request) {
-                        Request sendRequest = (Request) readObj;
-                        System.out.println(sendRequest.toString());
-                        if (sendRequest.isRequest()) {
-                            System.out.println("sender: " + sendRequest.isRequest());
-                            sendRequestToPlayer(sendRequest.getReceiverUserName(), sendRequest);
-                        } else {
-                            System.out.println("receiver: " + sendRequest.isResponse());
-                            sendRequestToPlayer(sendRequest.getSendingUserName(), sendRequest);
-                        }
+                } else if (readObj instanceof Register) {
+                    Register r = (Register) readObj;
+                    System.out.println("userName: " + r.getUserName() + ", pass: " + r.getPassward());
+                    try {
+                        Player p = db.registerNewPlayer(r);
+                        oos.writeObject(p);
+                        oos.flush();
+                    } catch (SQLException sQLException) {
+                        System.out.println(sQLException.toString());
+                        oos.writeObject("Error");
+                        oos.flush();
+                    }
+                } else if (readObj instanceof TopOnlinePlayers) {
+                    TopOnlinePlayers topOnlinePlayers = (TopOnlinePlayers) readObj;
+                    try {
+                        String str = topOnlinePlayers.getUserName();
+                        topOnlinePlayers.setOnlinePlayers(db.getOnlinePlayers(str));
+                        topOnlinePlayers.setTopPlayers(db.getTopPlayers());
+                        oos.writeObject(topOnlinePlayers);
+                        oos.flush();
+                    } catch (SQLException sQLException) {
+                        System.out.println(sQLException.toString());
+                        oos.writeObject("error 4");
+                        oos.flush();
+                    }
+                } else if (readObj instanceof LogOut) {
+                    Player p = new Player();
+                    LogOut logOut = (LogOut) readObj;
+                    System.out.println(logOut.getUserName());
+                    p.setUserName(logOut.getUserName());
+                    p.setIsRequest(0);
+                    p.setIsOnline(0);
+                    try {
+                        System.out.println("in try");
+                        db.changeOnlineStatus(p);
+                        db.inGameStatus(p);
+                        System.out.println(oos.toString());
+                        oos.writeObject("LoggedOut");
+                        System.out.println(socket);
+                        oos.flush();
+                        ois.close();
+                        oos.close();
+                        socket.close();
+                        clientsVector.remove(p.getUserName());
+                    } catch (SQLException ex) {
+                        Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+                        System.out.println("something wrong");
+                    }
+                } else if (readObj instanceof Request) {
+                    Request sendRequest = (Request) readObj;
+                    System.out.println(sendRequest.toString());
+                    if (sendRequest.isRequest()) {
+                        System.out.println("sender: " + sendRequest.isRequest());
+                        sendRequestToPlayer(sendRequest.getReceiverUserName(), sendRequest);
+                    } else {
+                        System.out.println("receiver: " + sendRequest.isResponse());
+                        sendRequestToPlayer(sendRequest.getSendingUserName(), sendRequest);
+                    }
+                } else if (readObj instanceof Move) {
+                    Move move = (Move) readObj;
+                    System.out.println("m1:" + move.getPlayer1());
+                    System.out.println("m2" + move.getPlayer2());
+                    if (move.isTurn()) {
+                        System.out.println("sender: " + move.isTurn());
+                        sendMoveToPlayer(move.getPlayer1(), move);
+                    } else {
+                        System.out.println("receiver: " + move.isTurn());
+                        sendMoveToPlayer(move.getPlayer2(), move);
                     }
                 }
-
             } catch (SocketException s) {
 //                     try {
 //                    is.close();
@@ -182,6 +188,7 @@ public class ClientHandler extends Thread {
                 Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+
     }
 
     public synchronized void sendRequestToPlayer(String userName, Request r) {
@@ -189,6 +196,20 @@ public class ClientHandler extends Thread {
             ClientHandler ch = clientsVector.get(userName);
             System.out.println("Receiver: " + ch.socket);
             ch.oos = new ObjectOutputStream(ch.os);
+            System.out.println(ch.oos);
+            ch.oos.writeObject(r);
+            ch.oos.flush();
+        } catch (IOException ex) {
+            Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex);
+        }
+    }
+
+    public synchronized void sendMoveToPlayer(String userName, Move r) {
+        System.out.println(userName);
+        try {
+            ClientHandler ch = clientsVector.get(userName);
+            System.out.println("Receiver: " + ch.socket);
             System.out.println(ch.oos);
             ch.oos.writeObject(r);
             ch.oos.flush();
--- src/model/Move.java
+++ src/model/Move.java
@@ -11,8 +11,41 @@ import java.io.Serializable;
  *
  * @author Azza Helmy
  */
-public class Move implements Serializable{
+public class Move implements Serializable {
+
     private int x;
+    private String player1;
+    private String player2;
+    private boolean turn;
+
+    public String getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(String player1) {
+        this.player1 = player1;
+    }
+
+    public String getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(String player2) {
+        this.player2 = player2;
+    }
+
+    public Move(String player1, String player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public boolean isTurn() {
+        return turn;
+    }
+
+    public void setTurn(boolean turn) {
+        this.turn = turn;
+    }
 
     public Move(int x) {
         this.x = x;
